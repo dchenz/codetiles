8755aefec087c2256fde8f6a3a893bc71233fba4
chendann <51312422+chendann@users.noreply.github.com>
2022-04-13 22:46:18 +1000

    Function declare only needs body connector

--- src/Model/Tiles/FunctionDeclare.ts
+++ src/Model/Tiles/FunctionDeclare.ts
@@ -4,15 +4,13 @@ export class FunctionDeclare extends ProgramObject {
 
   constructor(title?: string) {
     super("function_declare", title ?? "");
-    this.addConnector("inside", "INSIDE");
-    this.addConnector("outside", "OUTSIDE");
+    this.addConnector("body", "BODY");
     this.setAttribute("name", "");
   }
 
   toObject(): Record<string, unknown> {
     const objRep = super.toObject();
-    objRep["inside"] = super.getOutboundConnector("inside")?.targetId;
-    objRep["outside"] = super.getOutboundConnector("outside")?.targetId;
+    objRep["body"] = super.getOutboundConnector("body")?.targetId;
     return objRep;
   }
 
